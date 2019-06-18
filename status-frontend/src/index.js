4d8d116d2cd8f68ac11c08224b48ccbbdf315eca
Jasterix <ljcalixte@gmail.com>
2019-06-18 11:12:47 -0400

    morning start 11am

--- status-frontend/src/index.js
+++ status-frontend/src/index.js
@@ -2,23 +2,30 @@
 
 // url =
 
-const ulTag = document.querySelector('#status-container')
+const mainTag = document.querySelector('main')
+const imgDiv = mainTag.querySelector('div')
 
 const createTile =(gif) => {
-  return(`<li>
-    <img src = "${gif.img_url}">
-    </li>`)
+  return(`<a class="red card">
+    <div class ="image ui list">
+    <img class="ui medium image" src ="${gif.images.original.url}">
+    </div>
+    </a>`)
   }
 
+    // <div class ="ui list">
+    // <img class="ui small image" src = "${gif.images.original.url}">
+    // </div>
 
-fetch('https://api.giphy.com/v1/gifs/search?api_key=HDgQlOddDbNfFqqzsbHvWi17CPZ6X4JP&q=success cat&limit=4&offset=0&rating=G&lang=en')
+
+fetch('https://api.giphy.com/v1/gifs/search?api_key=HDgQlOddDbNfFqqzsbHvWi17CPZ6X4JP&q=404&limit=2&offset=0&rating=G&lang=en')
     // .then(parsed => parsed.data[getRandomInt(0,100)].images.original.url)
     .then(response => response.json())
     .then(data => {
       // debugger
       data.data.forEach(gif => {
 
-        ulTag.innerHTML += createTile(gif)
+        imgDiv.innerHTML += createTile(gif)
       })
     })
 
